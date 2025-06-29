55898f796ba0d22e074ff8c8e6314d2ef972d59f
Iochem <brendanunesdeandradeiochem@gmail.com>
2025-06-29 16:53:27 -0300

    feat:estrutura inicial sistema de gerenciamento de usuários

---
+++ src/main/java/com/iochem/sistemaGerenciamento/entity/UsuarioEntity.java
@@ -0,0 +1,52 @@
+package com.test.demo.entity;
+
+
+import com.test.demo.dto.UsuarioDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Objects;
+
+/*
+@Getter -Gera automaticamente os métodos get para todos os atributos.
+@Setter - Gera automaticamente os métodos set para todos os atributos.
+@NoArgsConstructor — gera um construtor sem argumentos.
+@AllArgsConstructor — gera um construtor com todos os campos como parâmetros.
+@Table() -Informa qual nome de tabela para esse objeto
+@Id - Anotação do JPA -  indica qual atributo chave primária da tabela
+*/
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "NPL_USUARIO")
+public class UsuarioEntity {
+
+    //Chave primária
+    @Id
+    //Indica que o id será gerado automaticamente pelo banco de dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //Informa que esse campo não pode ser nulo
+   @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)//unique = true - não ter usuário repetido
+    private String login;
+
+    @Column(nullable = false)
+    private String gmail;
+
+    public UsuarioEntity(UsuarioDto usuario){
+        BeanUtils.copyProperties(usuario, this);
+    }
+
+}
