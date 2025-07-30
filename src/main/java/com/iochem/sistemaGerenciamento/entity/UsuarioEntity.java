3ebb43426f2f147cc0d77acc77acf26b1ac0416d
Iochem <brendanunesdeandradeiochem@gmail.com>
2025-07-30 08:50:11 -0300

    fix(entity): adiciona os campos esquecidos na classe UsuarioEntity

--- src/main/java/com/iochem/sistemaGerenciamento/entity/UsuarioEntity.java
+++ src/main/java/com/iochem/sistemaGerenciamento/entity/UsuarioEntity.java
@@ -1,4 +1,29 @@
 package com.iochem.sistemaGerenciamento.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "s_usuario")
 public class UsuarioEntity {
+
+    @Id
+    //Indica que o id será gerado automaticamente pelo banco de dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //Campo não pode ser nulo
+    @Column(nullable = false, unique = true)//unique = true - não ter usuário repetido
+    private String login;
+
+    @Column(nullable = false)
+    private String gmail;
+
 }
